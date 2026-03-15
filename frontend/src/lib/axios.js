ab3d02ef9ee7b809cf083e8bf867eafe814eaa8b
Vili <vili528@gmail.com>
2026-03-15 19:58:28 +0100

    try again

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
 const BASE_URL =
-  // import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";  dev
-  (import.meta.env.MODE = "https://project01-full-react.onrender.com"); //production
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://viliweb.hu";
+dev;
+//(import.meta.env.MODE = "https://project01-full-react.onrender.com"); //production
 
 const api = axios.create({
   baseURL: BASE_URL,
