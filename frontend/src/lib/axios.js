47384e68844f95bf13d5bda48f4a143ef67734c7
Vili <vili528@gmail.com>
2026-03-15 20:03:52 +0100

    try

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,7 +5,7 @@ const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
     : "https://project01-full-react.onrender.com";
-dev;
+//dev;
 //(import.meta.env.MODE = "https://project01-full-react.onrender.com"); //production
 
 const api = axios.create({
