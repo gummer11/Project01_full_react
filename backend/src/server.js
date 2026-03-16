a83cfe17d9a7a72e3b741090d773bd3ebc14ceea
Vili <vili528@gmail.com>
2026-03-16 14:34:35 +0100

    ok

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,12 +15,15 @@ connectDB();
 //middleware
 app.use(
   cors({
-    // origin: "http://localhost:5173", //dev
-
+    //origin: "http://localhost:5173", //dev
+    origin:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5173/api"
+        : "https://viliweb.hu",
     //az alábbi a vercel production
-    origin: "https://viliweb.hu", // A Vercel-es URL-ed
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    //origin: "https://viliweb.hu", // A Vercel-es URL-ed
+    //methods: ["GET", "POST", "PUT", "DELETE"],
+    //credentials: true,
   })
 );
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
